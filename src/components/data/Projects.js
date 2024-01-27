6528f9c32aec3d0bda7bec48a6faa2d9ffa4219e
Fawad Ahmad <fawad7998@gmail.com>
2024-01-27 12:59:12 +0500

    First commit

---
+++ src/components/data/Projects.js
@@ -0,0 +1,124 @@
+// import Jungle from './../../img/Jungle.jpg';
+// import Books from './../../img/Books2.jpg';
+// import Hotel from './../../img/Hotel.jpg';
+// import Marvel from './../../img/Marvel.jpg';
+// import News from './../../img/News.jpg';
+// import Shiny from './../../img/Shiny.jpg';
+// import Portfolio from './../../img/Portfolio.jpg';
+// import WorkInProgress from './../../img/WorkInProgress.jpg';
+// import AlexShopEcommerce from './../../img/Alex_Shop_Ecommerce.jpg';
+// import abc from './../../img/abc.jpg';
+
+export const projects = {
+  collaborations: [
+    {
+      name: 'Google Developer Student Clubs',
+      time: '3 months',
+      projects: [],
+      position: 'Chrome Extension Developer',
+      company: 'Google Developer Student Clubs',
+      companyLink: 'https://developers.google.com/community/gdsc',
+      address: 'India',
+      work: 'Worked on Manifest V3 and JavaScripts to develop robust and scalable extension..',
+    },
+    {
+      name: 'IIITian Networks',
+      time: '8 Months',
+      projects: [],
+      position: 'Web Developer',
+      company: 'IIITian Networks',
+      companyLink: 'https://www.linkedin.com/company/iiitians-network/',
+      address: 'India',
+      work: 'Worked in front-end team and helped in debugging and development of dynamic and responsive website using HTML, CSS, JavaScript..',
+    },
+  ],
+  personnalProjects: [
+    {
+      name: 'G.Ed. Tech.',
+      //   img: abc,
+      live_demo_url: 'https://gaurav-edtech.vercel.app/',
+      gitHub_url: 'https://github.com/gaurav-101/gedtech',
+      technologies: ['#Redux', '#mongoDB', '#razorpay'],
+      colSize: 12,
+      summary:
+        'Developed a scalable educational platform, GEd.Tech., on the MERN stack (MongoDB, ExpressJS, ReactJS, Redux, NodeJS) for enhanced learning experiences. Developed user-friendly interfaces, including features like course creation, checkout flow with Razorpay payment gateway integration, and cloud-based media management.',
+    },
+    {
+      name: '5Star Movie Rating',
+      //   img: AlexShopEcommerce,
+      live_demo_url: 'https://star-movie-rating.vercel.app/',
+      gitHub_url: 'https://github.com/gaurav-101/movie-review',
+      technologies: ['#nextJS', '#mongoDB', '#paypal'],
+      colSize: 12,
+      summary:
+        'Designed complete backend API with Node.js, Express, TailwindCSS, MongoDB, Cloudinary. Role Based Authentication (Admin/ Normal User) with Email Verification. Advance Form with validation, live search fields and UI with React and Context API',
+    },
+    {
+      name: 'Site web Portfolio',
+      //   img: Portfolio,
+      live_demo_url: 'https://gaurav-v-portfolio.vercel.app/',
+      gitHub_url: 'https://github.com/gaurav-101/Portfolio',
+      technologies: ['#nextJS', '#tailwindcss', '#framer-motion'],
+      colSize: 6,
+      summary:
+        'Responsive portfolio website created using NextJS, framer-motion and TailwindCSS. It has animations and transitions. It lists all the projects I have created.',
+    },
+    {
+      name: 'Edemy-Learning Platform',
+      //   img: Books,
+      live_demo_url: 'https://github.com/gaurav-101/Edemy',
+      gitHub_url: 'https://github.com/gaurav-101/Edemy',
+      technologies: ['#ReactJS', '#Redux', '#TypeScript'],
+      colSize: 6,
+      summary:
+        'The Resource and Learning Website aims to provide a user-friendly and intuitive interface that offers a vast array of educational materials, ranging from articles, videos.',
+    },
+    {
+      name: 'Discord Clone',
+      //   img: Shiny,
+      live_demo_url: 'https://discord-cloning.netlify.app/',
+      gitHub_url: 'https://github.com/gaurav-101/Discord_clone',
+      technologies: ['#HTML', '#Javascript', '#TailwindCSS'],
+      colSize: 6,
+      summary:
+        'Created a Discord Website clone with responsiveness using HTML and Tailwind_CSS. 🚀🚀',
+    },
+    {
+      name: 'Weather App',
+      //   img: Jungle,
+      live_demo_url: 'https://weatherapple.netlify.app/',
+      gitHub_url: 'https://github.com/gaurav-101/Weather',
+      technologies: ['#Javascript', '#HTML5', '#CSS3'],
+      colSize: 6,
+      summary:
+        'Built a Weather App using HTML, CSS, and JavaScript! Used a free API to fetch the location data and created a responsive website.',
+    },
+    {
+      name: 'We Meet',
+      //   img: Marvel,
+      live_demo_url: 'https://wemeet.onrender.com/',
+      gitHub_url: 'https://github.com/gaurav-101/weMeet',
+      technologies: ['#Node Js', '#Webrtc', '#Socket.io'],
+      colSize: 12,
+      summary: 'Interaction platform using socket.io and webrtc. ',
+    },
+    {
+      name: 'News Website',
+      //   img: News,
+      live_demo_url: '#',
+      gitHub_url: '#',
+      technologies: ['#HTML5', '#CSS3'],
+      colSize: 6,
+      summary: 'Front-end template for an information website created in HTML',
+    },
+    {
+      name: 'Hotel Website',
+      //   img: Hotel,
+      live_demo_url: '#',
+      gitHub_url: '#',
+      technologies: ['#HTML5', '#CSS3'],
+      colSize: 6,
+      summary: 'Front-end template for a hotel website created in HTML',
+    },
+  ],
+};
